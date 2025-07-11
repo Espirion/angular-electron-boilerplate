c826ff9e5e00c57cc8877d37215c4ce763226d33
Kothapalli Somasekhar <somunani2413@gmail.com>
2025-07-12 01:03:32 +0530

    Adding @angular/material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatDialogModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-electron-boilerplate';
 
-  ngOnInit(): void {
-    console.log('AppComponent initialized');
-  }
+  ngOnInit(): void {}
 }
